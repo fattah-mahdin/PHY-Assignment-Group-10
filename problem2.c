cc936efa9bc97465fc8010d9dd4e412aa8cf5a4f
Fattah Mahdin <fattah3100@gmail.com>
2025-07-24 00:14:11 +0600

    Update problem2.c

--- problem2.c
+++ problem2.c
@@ -5,13 +5,9 @@ int main() {
     int m1 = 3, m2 = 2;
     double lambda1 = 530; 
     double lambda2 = 700; 
-    double theta1_deg = 65.0;
-
-    
-    double theta1_rad = theta1_deg * 3.1416/ 180.0;
-
+    double theta1_degree = 65.0;
+    double theta1_rad = theta1_degree * 3.1416/ 180.0;
     double d = (m1 * lambda1) / sin(theta1_rad); 
-
     double sin_theta2 = (m2 * lambda2) / d;
 
     if (sin_theta2 > 1.0) {
